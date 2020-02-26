bc2a5ff22184dec263dedd80808435d7591de044
Hector Flores <Hector_Flores@eogresources.com>
2020-02-26 15:45:16 -0700

    Challenge: using async/await

---
+++ src/async/challenge.js
@@ -0,0 +1,19 @@
+const fetchData = require("../utils/fetchData");
+const API = "http://rickandmortyapi.com/api/character/";
+
+const anotherFunction = async url_api => {
+  try {
+    const data = await fetchData(url_api);
+    const character = await fetchData(`${API}${data.results[0].id}`);
+    const origin = await fetchData(character.origin.url);
+    console.log(data.info.count);
+    console.log(character.name);
+    console.log(origin.dimension);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+console.log(`before`);
+anotherFunction(API);
+console.log(`after`);
